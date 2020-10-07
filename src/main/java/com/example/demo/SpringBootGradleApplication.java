bc30817ab579d3e11d5575709a7db2b2742c79b2
Patra <uma_patra@optum.com>
2020-10-07 16:25:05 +0530

    add gradle code

---
+++ src/main/java/com/example/demo/SpringBootGradleApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleApplication.class, args);
+		System.out.println("-->>>>>>>>>> :: Main Method :: ");
+	}
+
+}
